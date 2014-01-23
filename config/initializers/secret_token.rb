642289c7aeb4db5e738267e12705df00ab1b930a
missmaggiemo <margaret.m.moreno@gmail.com>
2014-01-22 17:37:33 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MaggieApp::Application.config.secret_key_base = '5b26d6809d57e229c01ca819aea7456328842a29fd85a90f4070095e1b09e34c627971faca7018ace998e4559e2b1470415257a674b4208d687659389a00b48c'
