90e3e849c676cb50935f98e99dd6ce08c0de17b9
missmaggiemo <margaret.m.moreno@gmail.com>
2014-05-03 10:51:07 -0700

    random thing

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,25 @@
 module StaticPagesHelper
+  
+  def random_thing
+    ["likes comfortable chairs", 
+      "loves the color blue", 
+      "wants to get sushi right now", 
+      "orders tea, Earl Grey, hot at bars", 
+      "devours information like movie theater popcorn", 
+      "sometimes goes running",
+      "doesn't believe in magic",
+      "is still looking for a way to do all her work sitting outside",
+      "prefers cold weather",
+      "knows most of the rules in rugby",
+      "is terrible at chess",
+      "likes long walks on the beach",
+      "prefers sunrises to sunsets",
+      "has seen the proof that all natural numbers sum to -1/12 and believes it",
+      "didn't discover Reddit until she was 23",
+      "can't play the piano",
+      "< insert funny phrase here >",
+      "is entirely fueled by cheese curls"
+    ].sample
+  end
+  
 end
